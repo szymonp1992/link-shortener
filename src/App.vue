b64a9da7cdb9dbfd7c32816839bf0adda3212998
Szymon Podstufka (Prywatne) <szymonpodstufka@MBP-Szymon.kopnet.pl>
2022-11-14 22:09:53 +0100

    Added copying functionality to generated short link

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,45 @@
 <template>
-  <form action="" @submit.prevent="getLink">
+  <div>
+    <form action="" @submit.prevent="getLink">
+      <input
+        type="text"
+        name="link"
+        id="link"
+        placeholder="Your URL"
+        v-model="link"
+      />
+      <button>Shorten URL</button>
+    </form>
     <input
       type="text"
-      name="link"
-      id="link"
-      placeholder="Your URL"
-      v-model="link"
+      id="shortURL"
+      placeholder="Your shortened URL"
+      :value="shortLink"
     />
-    <button>Shorten URL</button>
-    <input type="text" placeholder="Your shortened URL" :value="shortLink">
-  </form>
+    <button class="btn" @click="copyShortUrl" data-clipboard-target="#shortURL">
+      Copy
+    </button>
+    <p v-if="isActive">Copied</p>
+  </div>
 </template>
-
 
 
 
 <script>
 import { ref } from "vue";
-// import {useStore} from 'vuex'
 
 export default {
   setup() {
     const link = ref("");
-    const shortLink = ref("")
+    const shortLink = ref("");
+    const isActive = ref(false);
 
     async function getLink() {
+      isActive.value = false;
       const response = await fetch("https://api-ssl.bitly.com/v4/shorten", {
         method: "POST",
         headers: {
-          "Authorization": 'Bearer 6e9551a514b1f88c27de0f92bcb9bd956baaf8e7',
+          Authorization: "Bearer 6e9551a514b1f88c27de0f92bcb9bd956baaf8e7",
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
@@ -38,12 +49,15 @@ export default {
       });
       const data = await response.json();
       shortLink.value = data.link;
-
     }
 
-    //Access token: 6e9551a514b1f88c27de0f92bcb9bd956baaf8e7
+    const clipboard = new ClipboardJS('.btn');
 
-    return { link, shortLink, getLink };
+    clipboard.on('success', () => {
+        isActive.value = true;
+      });
+
+    return { link, shortLink, isActive, getLink };
   },
 };
 </script>
@@ -57,7 +71,7 @@ export default {
   font-size: 2rem;
 }
 
-form {
+div {
   position: absolute;
   top: 50%;
   left: 50%;
@@ -65,22 +79,35 @@ form {
   width: 50%;
 }
 
-form button {
+button {
   color: white;
   background-color: orange;
   border: none;
   border-radius: 5px;
-  padding: 0.5rem;
+  padding: 0.5rem 2rem;
   font-weight: bold;
   display: block;
-  margin: 30px auto;
+  margin: 1rem auto;
+}
+
+button:hover {
+  cursor: pointer;
 }
 
 input {
-  height: 3rem;
   display: block;
   border: 2px solid orange;
   border-radius: 5px;
   width: 100%;
+  padding: 1rem;
 }
+
+p {
+  position: relative;
+  font-size: 1rem;
+  text-align: center;
+  margin: 0;
+}
+
+
 </style>
