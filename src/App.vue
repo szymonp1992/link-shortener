a15aaf3f925bfcd193210ff74b2e3875e80ec487
Szymon Podstufka (Prywatne) <szymonpodstufka@MBP-Szymon.kopnet.pl>
2022-11-14 20:10:03 +0100

    Shortening URL functionality finished

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,86 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <form action="" @submit.prevent="getLink">
+    <input
+      type="text"
+      name="link"
+      id="link"
+      placeholder="Your URL"
+      v-model="link"
+    />
+    <button>Shorten URL</button>
+    <input type="text" placeholder="Your shortened URL" :value="shortLink">
+  </form>
 </template>
 
+
+
+
+<script>
+import { ref } from "vue";
+// import {useStore} from 'vuex'
+
+export default {
+  setup() {
+    const link = ref("");
+    const shortLink = ref("")
+
+    async function getLink() {
+      const response = await fetch("https://api-ssl.bitly.com/v4/shorten", {
+        method: "POST",
+        headers: {
+          "Authorization": 'Bearer 6e9551a514b1f88c27de0f92bcb9bd956baaf8e7',
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          long_url: link.value,
+          domain: "bit.ly",
+        }),
+      });
+      const data = await response.json();
+      shortLink.value = data.link;
+
+    }
+
+    //Access token: 6e9551a514b1f88c27de0f92bcb9bd956baaf8e7
+
+    return { link, shortLink, getLink };
+  },
+};
+</script>
+
+
+
+
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+* {
+  font-family: Roboto, sans-serif;
+  font-size: 2rem;
 }
 
-.logo {
+form {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 50%;
+}
+
+form button {
+  color: white;
+  background-color: orange;
+  border: none;
+  border-radius: 5px;
+  padding: 0.5rem;
+  font-weight: bold;
   display: block;
-  margin: 0 auto 2rem;
+  margin: 30px auto;
 }
 
-nav {
+input {
+  height: 3rem;
+  display: block;
+  border: 2px solid orange;
+  border-radius: 5px;
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
 }
 </style>
